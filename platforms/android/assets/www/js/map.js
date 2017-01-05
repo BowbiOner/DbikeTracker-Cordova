9e923c7312272cbffe4c72fa9515f8cbe06671d6
Aaron O'Hare <aaronoh96@gmail.com>
2017-01-05 14:39:29 +0000

    Calculating straight line distance between user and all stations, begining implementation of distance matrix (had to do striaght line distance first due to 25 origin limit on Distance Matrix)

--- platforms/android/assets/www/js/map.js
+++ platforms/android/assets/www/js/map.js
@@ -19,6 +19,12 @@ var app = {
     },
 
     onSuccess: function(position) {
+        //var myLng = position.coords.longitude;
+        //var myLat = position.coords.latitude;
+        var myLng = -6.260778899999991;
+        var myLat = 53.35124159999999;
+        var myLatLong = new google.maps.LatLng(myLat, myLng);
+
         //var liveUrl "http://dbiketrackerv2.herokuapp.com/php-scripts/live-data.php";
         var url = "http://dbiketrackerv2.herokuapp.com/view-locations-sql.php";
 
@@ -47,28 +53,45 @@ var app = {
 
             });
 
+            number = result.map(function(a) {
+                return a.NUMBER;
+
+            });
+
             var points = [];
 
             $(lats).each(function(index, val) {
-                points.push([lats[index], longs[index], names[index], avail[index], availslts[index]]);
+                points.push([lats[index], longs[index], names[index], avail[index], availslts[index], number[index]]);
             })
-            console.log("Bikes Available: " + avail);
-            console.log("Names: " + names);
-            centreLatLong = new google.maps.LatLng(points[10][0], points[10][1]);
 
             var mapOptions = {
-                center: centreLatLong,
-                zoom: 17,
+                center: myLatLong,
+                zoom: 15,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
 
             var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+            var myContentString = "<p>" + " This is your current location " + "<br />" + "Closest Station: Drive" + "</p>";
+
+            var myMarker = new google.maps.Marker({
+                icon: './img/current_location.png',
+                position: myLatLong,
+                map: map,
+                myContentString: myContentString
+            });
+            var myInfoWindow = new google.maps.InfoWindow({});
+
+            myMarker.addListener('click', function() {
+                myInfoWindow.setContent(this.myContentString);
+                myInfoWindow.open(map, this);
+            });
+
             for (var i = 0; i < points.length; i++) {
                 longitude = longs[i];
                 latitude = lats[i];
                 latLong = new google.maps.LatLng(points[i][0], points[i][1]);
-                var contentString = "</p>" + points[i][2] + "<br />" + " Bikes Available:" + points[i][3] + "<br />" + " Slots Available:" + points[i][4];
+                var contentString = "<p>" + points[i][2] + "<br />" + " Bikes Available:" + points[i][3] + "<br />" + " Slots Available:" + points[i][4] + "</p>";
 
                 var marker = new google.maps.Marker({
                     position: latLong,
@@ -85,18 +108,84 @@ var app = {
 
 
             }
+            // console.log("Station Lat: " + points[25][0], "Station Lng: " + points[25][1]);
+            // console.log("My Lat: " + myLat + "My Lng: " + myLng);
 
+            var mLocation = new google.maps.LatLng(myLat, myLng);
+
+            var sLocations = [];
+            for (var i = 0; i < points.length; i++) {
+                sLocation = new google.maps.LatLng(points[i][0], points[i][1]);
+
+                var distanceFromAllStations = google.maps.geometry.spherical.computeDistanceBetween(mLocation, sLocation);
+
+                sLocations.push([points[i][5], distanceFromAllStations]);
+
+            }
+            var nearMe = [];
+            for (var i = 0; i < sLocations.length; i++) {
+                if (sLocations[i][1] <= 750) {
+                    nearMe.push(sLocations[i]);
+                }
+            }
+
+
+            console.log(nearMe);
+
+            var bounds = new google.maps.LatLngBounds;
+            var geocoder = new google.maps.Geocoder;
+
+            var service = new google.maps.DistanceMatrixService;
+            service.getDistanceMatrix({
+                origins: [mLocation],
+                destinations: sLocations,
+                travelMode: 'DRIVING',
+                unitSystem: google.maps.UnitSystem.METRIC,
+                avoidHighways: false,
+                avoidTolls: false
+            }, function(response, status) {
+                if (status !== 'OK') {
+                    alert('Error was: ' + status);
+                } else {
+                    var originList = response.originAddresses;
+                    var destinationList = response.destinationAddresses;
+                    var outputDiv = document.getElementById('output');
+                    outputDiv.innerHTML = '';
+                    //deleteMarkers(markersArray);
+
+                    for (var i = 0; i < originList.length; i++) {
+                        var results = response.rows[i].elements;
+                        geocoder.geocode({
+                            'address': originList[i]
+                        });
+                        for (var j = 0; j < results.length; j++) {
+                            geocoder.geocode({
+                                'address': destinationList[j]
+                            });
+                            outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+                                ': ' + results[j].distance.text + ' in ' +
+                                results[j].duration.text + '<br>';
+                        }
+                    }
+                }
+            });
         });
+
+
+
+
     },
+
+
     /*
-	var line = new google.maps.Polyline({
-    path: [new google.maps.LatLng(37.4519, -122.1519), new google.maps.LatLng(53.280121, -6.152963)],
-    strokeColor: "#FF0000",
-    strokeOpacity: 1.0,
-    strokeWeight: 10,
-    geodesic: true,
-    map: map
-});*/
+    	var line = new google.maps.Polyline({
+        path: [new google.maps.LatLng(37.4519, -122.1519), new google.maps.LatLng(53.280121, -6.152963)],
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 10,
+        geodesic: true,
+        map: map
+    });*/
 
     onError: function(error) {
         alert("the code is " + error.code + ". \n" + "message: " + error.message);
